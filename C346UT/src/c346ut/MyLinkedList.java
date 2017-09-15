722e76f97d32ec63f78683746de160e727aed2c7
Steven <stevie1334@gmail.com>
2017-09-14 20:59:01 -0400

    Fixed all problems and incomplete work

--- C346UT/src/c346ut/MyLinkedList.java
+++ C346UT/src/c346ut/MyLinkedList.java
@@ -3,11 +3,7 @@ package c346ut;
 public class MyLinkedList<T> implements ILinkedList<T>{
     private ILinkedList<T> next;
     private T value;
-    private ILinkedList<T> first;
-   // @Override
-    //public String toString(){
-  //     return 
-   // }
+
     public MyLinkedList(T value){
         this.value = value;
     }
@@ -17,21 +13,19 @@ public class MyLinkedList<T> implements ILinkedList<T>{
     }
     @Override
     public ILinkedList<T> last(){
-       ILinkedList temp= this.next;
-        while(temp!=null){
-            temp.next();
+        while(next.next()!=null){
+            next=next.next();
         }
         return next;
     }
     
     @Override
     public ILinkedList<T> after(int n){
-           for (int i = 0; i < n; i++) {
+           for (int i = 0; i < n-1; i++) {
             next=next.next();
         }
            return next;
-    }
-        
+    }   
 
    // /**
    //  * Removes the next element (sets null)
@@ -40,54 +34,36 @@ public class MyLinkedList<T> implements ILinkedList<T>{
    @Override
     public ILinkedList<T> detach(){
         ILinkedList temp=next;
-        next=null;
+        this.next=null;
         return temp;
     }
 
-   // /**
-   //  * Gets the current value
-   //  * @return the current value
-   //  */
    @Override
     public T get(){
         return value;
     }
 
-   // /**
-   //  * Sets the value of this node
-   //  * @param value the new value
-   //  */
     @Override
     public void set(T value){
         this.value = value;
     }
 
-   // /**
-   //  * Sets the next element in the list
-   //  * @param next the next element
-   //  */
     @Override
     public void setNext(ILinkedList<T> next){
         this.next=next;
     }
 
-   // /**
-   //  * Sets the next element after this current element
-   //  * @param next the next element
-   //  */
     @Override
     public void append(ILinkedList<T> next){
         this.next=next();
     }
 
-   // /**
-   //  * Adds the current list as the next of newFirst
-   //  * @param newFirst the new head of the list
-   //  */
     @Override
     public void insert(ILinkedList<T> newFirst){
-        newFirst.setNext(first);
-		first = newFirst;
+        
+        newFirst.setNext(this);
+	
     }
+    
 }
 
--- C346UT/test/c346ut/MyLinkedListTest.java
+++ C346UT/test/c346ut/MyLinkedListTest.java
@@ -33,19 +33,112 @@ public class MyLinkedListTest {
     ILinkedList<Integer> second = new MyLinkedList(6);
     ILinkedList<Integer> third = new MyLinkedList(7);
     first.setNext(second);
-    second.setNext(third);
     
     first.next();
+    ILinkedList actual=first.next();
+    ILinkedList expected=second;
+    assertEquals(actual,expected);
+    
+    }
+    @Test
+   public void testLast() {
+    ILinkedList<Integer> first = new MyLinkedList(5);
+    ILinkedList<Integer> second = new MyLinkedList(6);
+    ILinkedList<Integer> third = new MyLinkedList(7);
+    first.setNext(second);
+    second.setNext(third);    
+   
+    first.last();
+    ILinkedList actual=first.last();
+    ILinkedList expected=third;
+    assertEquals(actual,expected);
+    } 
+   
+    @Test
+    public void testAfter() {
+    ILinkedList<Integer> first = new MyLinkedList(5);
+    ILinkedList<Integer> second = new MyLinkedList(6);
+    ILinkedList<Integer> third = new MyLinkedList(7);
+    first.setNext(second);
+    second.setNext(third);    
+   
+    first.after(2);
+    ILinkedList actual=first.after(1);
+    ILinkedList expected=third;
+    assertEquals(actual,expected);
     }
     
-    public void testLast() {
+    @Test
+    public void testDetach() {
     ILinkedList<Integer> first = new MyLinkedList(5);
     ILinkedList<Integer> second = new MyLinkedList(6);
     ILinkedList<Integer> third = new MyLinkedList(7);
     first.setNext(second);
     second.setNext(third);
     
-    first.last(); 
+   
+    first.detach();
+    ILinkedList actual=first.next();
+    ILinkedList expected=null;
+    assertEquals(actual,expected);
     }
     
+    @Test
+    public void testGet() {
+    ILinkedList<Integer> first = new MyLinkedList(5);
+    ILinkedList<Integer> second = new MyLinkedList(6);
+    ILinkedList<Integer> third = new MyLinkedList(7);
+    first.setNext(second);
+    second.setNext(third);
+    
+   
+    first.get();
+    Integer actual=first.get();
+    Integer expected=5;
+    assertEquals(actual,expected);
+    }
+    
+    @Test
+    public void testSet() {
+    ILinkedList<Integer> first = new MyLinkedList(5);
+    ILinkedList<Integer> second = new MyLinkedList(6);
+    ILinkedList<Integer> third = new MyLinkedList(7);
+    first.setNext(second);
+    
+   
+    first.set(8);
+    Integer actual=first.get();
+    Integer expected=8;
+    assertEquals(actual,expected);
+    }
+    
+    @Test
+    public void testAppend() {
+    ILinkedList<Integer> first = new MyLinkedList(5);
+    ILinkedList<Integer> second = new MyLinkedList(6);
+    ILinkedList<Integer> third = new MyLinkedList(7);
+    first.setNext(second);
+    
+   
+    first.append(second);
+    ILinkedList actual=first.next();
+    ILinkedList expected=second;
+    assertEquals(actual,expected);
+    }
+    
+    @Test
+    public void testInsert() {
+    ILinkedList<Integer> first = new MyLinkedList(5);
+    ILinkedList<Integer> second = new MyLinkedList(6);
+    ILinkedList<Integer> third = new MyLinkedList(7);
+    ILinkedList<Integer> fourth = new MyLinkedList(8);
+    first.setNext(second);
+    second.setNext(third);
+    
+   
+    first.insert(fourth);
+    ILinkedList actual=fourth.next();
+    ILinkedList expected=first;
+    assertEquals(actual,expected);
+    }
 }
